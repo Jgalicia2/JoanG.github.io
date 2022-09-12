69127fceab16474f992f15995a2b4c7b6d1daab6
Joan <jgalicia2@student.gsu.edu>
2022-09-12 17:07:07 -0400

    Added resume to its page and fixed some styling

--- my-app/src/components/Contact.jsx
+++ my-app/src/components/Contact.jsx
@@ -1,9 +1,10 @@
 //Contact information page(edit needed)
-//TODO: add interesting features
+//TODO: maybe dont make a contact page this is sort of redundant unless you find another feature to add that brings life to this page
 const Contact = () => {
     return (
         <div>
             <h1>Contact Information</h1>
+
         </div>
     );
 };
--- my-app/src/components/Resume.jsx
+++ my-app/src/components/Resume.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-//TODO: upload resume or find some interesting way of using this page
+import pdf from "./documents/JG_Resume.pdf";
+//TODO: This is great but ben lin did it better
+//find a better way of designing this
 const Resume = () => {
     return (
         <div>
             <h1> Resume </h1>
+            <div>
+                <iframe title="Resume" src={pdf} width="500px" height="700px">This is the Resume</iframe>
+            </div>
+            <a href={pdf}>Get a better View </a>
         </div>
     );
 };
